a14802ac68f340904cbdc5982aee75084487457f
Roman Begorre <bgr4435a@ens.fsi>
2023-03-28 17:05:38 +0200

    Fin TP2

--- src/personnages/Equipement.java
+++ src/personnages/Equipement.java
@@ -1,16 +1,19 @@
 package personnages;
 
 public enum Equipement {
-    CASQUE,
-    BOUCLIER
+    CASQUE("Casque"), BOUCLIER("Bouclier");
+	
+	private String nom;
+	private Equipement(String nom) {
+		this.nom = nom;
+	}
+	
+	String getNom() {
+		return nom;
+	}
+	
+	public String toString() {
+		return nom;
+	}
 }
 
-public class Test{
-	public static void main(String[] args) {
-	        Equipement equip1 = Equipement.CASQUE;
-	        Equipement equip2 = Equipement.BOUCLIER;
-	        
-	        System.out.println("Le premier équipement est un " + equip1);
-	        System.out.println("Le deuxième équipement est un " + equip2);
-	    }
-}
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -37,17 +37,51 @@ public class Romain {
 		}
 	}
 	
-	public static void main(String[] args) {
+	public static void main1(String[] args) {
 		
 		Romain minus = new Romain("Minus", 6);
 		System.out.println(minus.prendreParole());
 		minus.parler("Je suis Minus !");
 		minus.recevoirCoup(9);
-		
-	
-		
-		
-		
 		}
+	
+	public static void main(String[] args) {
+		System.out.println(Equipement.CASQUE);
+		System.out.println(Equipement.BOUCLIER);
+		Romain minus = new Romain("Minus", 2);
+		Equipement casque = Equipement.CASQUE;
+		Equipement bouclier = Equipement.BOUCLIER;
+		
+		minus.sEquiper(casque);
+		minus.sEquiper(casque);
+		minus.sEquiper(bouclier);
+		minus.sEquiper(casque);
+	}
+	
+	private Equipement[] equipements = new Equipement[2];
+	private int nbEquipement = 0;
 
-}
+	
+	public void sEquiper(Equipement e) {
+		switch (nbEquipement){
+		case 0:
+			equipements[0] = e;
+			nbEquipement++;
+			System.out.println("Le soldat " + nom + " s'equipe avec un " + e.getNom() + ".");
+			break;
+		case 1:
+			if (equipements[0] == e) {
+				System.out.println("Le soldat " + nom + " posséde déja " + e.getNom() + ".");
+			}
+			else {
+				equipements[1] = e;
+				nbEquipement++;
+				System.out.println("Le soldat " + nom + " s'equipe avec un " + e.getNom() + ".");
+			}break;
+			
+		case 2:
+			System.out.println("Le soldat" + nom + " est déjà bien protégé !");
+				
+			}
+		}
+	}
